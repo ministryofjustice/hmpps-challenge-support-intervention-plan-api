dc72dc1b5bd595701ea3a9007bb53cb9fe8005f5
James Reed <james.reed@digital.justice.gov.uk>
2024-06-18 07:57:12 +0100

    chore: fix sql

--- src/main/resources/migration/common/V9__nullable_referral_fields.sql
+++ src/main/resources/migration/common/V9__nullable_referral_fields.sql
@@ -1,3 +1,3 @@
-alter table referral alter column incident_involvement_id set not null;
-alter table referral alter column description_of_concern set not null;
-alter table referral alter column known_reasons set not null;
+alter table referral alter column incident_involvement_id drop not null;
+alter table referral alter column description_of_concern drop not null;
+alter table referral alter column known_reasons drop not null;
