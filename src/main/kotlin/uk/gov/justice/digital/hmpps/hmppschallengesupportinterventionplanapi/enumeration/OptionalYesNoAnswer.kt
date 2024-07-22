7dca88506ebdc2e3a1e943d8857f39a7a5747ea5
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-07-22 09:56:12 +0100

    MIC-322 update so that swagger doc matches actual behaviour (#68)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/enumeration/OptionalYesNoAnswer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/enumeration/OptionalYesNoAnswer.kt
@@ -1,18 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import io.swagger.v3.oas.annotations.media.Schema
-
-@Schema(
-  type = "String",
-  allowableValues = [
-    "yes",
-    "no",
-    "do-not-know",
-  ],
-)
-enum class OptionalYesNoAnswer(@JsonProperty val code: String) {
-  YES("yes"),
-  NO("no"),
-  DO_NOT_KNOW("do-not-know"),
+enum class OptionalYesNoAnswer {
+  YES,
+  NO,
+  DO_NOT_KNOW,
 }
