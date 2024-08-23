5d9c05fa5c49f77e2619ceff7e9b70c211418d60
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2024-08-23 13:50:11 +0100

    added custom ordering and tests

--- src/main/resources/migration/common/V30__ref_data_list_sequence.sql
+++ src/main/resources/migration/common/V30__ref_data_list_sequence.sql
@@ -3,4 +3,67 @@ with ref_data as
           from reference_data rd
           where domain in (select distinct domain from reference_data))
 update reference_data rd
-set list_sequence = (select row_num from ref_data where ref_data.reference_data_id = rd.reference_data_id);
+set list_sequence = (select (row_num * 10) from ref_data where ref_data.reference_data_id = rd.reference_data_id);
+
+update reference_data
+    set list_sequence = 10
+    where domain = 'INCIDENT_INVOLVEMENT'
+      and code = 'PER';
+
+update reference_data
+    set list_sequence = 20
+    where domain = 'INCIDENT_INVOLVEMENT'
+      and code = 'VIC';
+
+update reference_data
+    set list_sequence = 30
+    where domain = 'INCIDENT_INVOLVEMENT'
+      and code = 'WIT';
+
+update reference_data
+    set list_sequence = 40
+    where domain = 'INCIDENT_INVOLVEMENT'
+      and code = 'OTH';
+
+update reference_data
+    set list_sequence = 10
+    where domain = 'OUTCOME_TYPE'
+      and code = 'CUR';
+
+update reference_data
+    set list_sequence = 20
+    where domain = 'OUTCOME_TYPE'
+      and code = 'OPE';
+
+update reference_data
+    set list_sequence = 30
+    where domain = 'OUTCOME_TYPE'
+      and code = 'WIN';
+
+update reference_data
+    set list_sequence = 40
+    where domain = 'OUTCOME_TYPE'
+      and code = 'ACC';
+
+update reference_data
+    set list_sequence = 50
+    where domain = 'OUTCOME_TYPE'
+      and code = 'NFA';
+
+update reference_data
+    set list_sequence = 10
+    where domain = 'DECISION_SIGNER_ROLE'
+      and code = 'CUSTMAN';
+
+update reference_data
+    set list_sequence = 20
+    where domain = 'DECISION_SIGNER_ROLE'
+      and code = 'THEHOFF';
+
+update reference_data
+    set list_sequence = 30
+    where domain = 'DECISION_SIGNER_ROLE'
+      and code = 'OTHER';
+
+alter table reference_data
+    add constraint unq_reference_data_domain_list_sequence unique (domain, list_sequence);
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/resource/ReferenceDataTranslationIntTest.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.resource
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.constant.ROLE_CSIP_UI
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration.ReferenceDataType
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.model.referenceData.ReferenceData
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+import java.time.LocalDateTime
+import java.util.Optional
+
+class ReferenceDataTranslationIntTest : IntegrationTestBase() {
+  @Test
+  fun `reference data items are bought back in sequence`() {
+    val referenceData = webTestClient.getReferenceData(ReferenceDataType.AREA_OF_WORK, null)
+    assertThat(referenceData).isNotEmpty()
+    assertThat(referenceData.map { it.listSequence }).isSorted()
+  }
+
+  @Test
+  fun `OUTCOME_TYPE reference data items are bought back in the correct sequence`() {
+    val referenceData = webTestClient.getReferenceData(ReferenceDataType.OUTCOME_TYPE, null)
+    assertThat(referenceData[0].code).isEqualTo("CUR")
+    assertThat(referenceData[1].code).isEqualTo("OPE")
+    assertThat(referenceData[2].code).isEqualTo("WIN")
+    assertThat(referenceData[3].code).isEqualTo("ACC")
+    assertThat(referenceData[4].code).isEqualTo("NFA")
+  }
+
+  private fun WebTestClient.getReferenceData(
+    referenceType: ReferenceDataType,
+    includeInactive: Boolean?,
+  ) = get().uri { builder ->
+    builder.path("/reference-data/${referenceType.domain}")
+      .queryParamIfPresent("includeInactive", Optional.ofNullable(includeInactive)).build()
+  }.headers(setAuthorisation(roles = listOf(ROLE_CSIP_UI))).exchange().expectStatus().isOk.expectHeader()
+    .contentType(MediaType.APPLICATION_JSON).expectBodyList(ReferenceData::class.java).returnResult().responseBody!!
+}
