c2f49706a548ea04da0a225b74cc6f21948e8f85
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-09-12 13:44:59 +0100

    MIC-546 fix tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/service/event/DomainEventPublisher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/service/event/DomainEventPublisher.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.service.event
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.entity.event.DomainEvent
@@ -19,21 +17,13 @@ class DomainEventPublisher(
   }
 
   fun publish(domainEvent: DomainEvent) {
-    runCatching {
-      domainEventsTopic.publish(
-        domainEvent.eventType,
-        objectMapper.writeValueAsString(domainEvent),
-        domainEvent.attributes(),
-      )
-    }.onFailure {
-      log.error("Failed to publish '$domainEvent'", it)
-    }
+    domainEventsTopic.publish(
+      domainEvent.eventType,
+      objectMapper.writeValueAsString(domainEvent),
+      domainEvent.attributes(),
+    )
   }
 
   private fun DomainEvent.attributes() =
     mapOf("eventType" to MessageAttributeValue.builder().dataType("String").stringValue(eventType).build())
-
-  private companion object {
-    private val log: Logger = LoggerFactory.getLogger(this::class.java)
-  }
 }
