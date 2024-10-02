64d6db28f2ebe88d5bee463114b539855c2ef821
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-02 12:30:06 +0100

    ktlintFormat

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/sync/NomisIdentifiable.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/sync/NomisIdentifiable.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.sync
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.config.SYSTEM_DISPLAY_NAME
@@ -38,6 +36,7 @@ class DefaultLegacyActioned(
 ) : LegacyActioned {
   @Schema(requiredMode = NOT_REQUIRED)
   override val actionedBy: String = actionedBy ?: SYSTEM_USER_NAME
+
   @Schema(requiredMode = NOT_REQUIRED)
   override val actionedByDisplayName: String = actionedByDisplayName ?: SYSTEM_DISPLAY_NAME
 }
