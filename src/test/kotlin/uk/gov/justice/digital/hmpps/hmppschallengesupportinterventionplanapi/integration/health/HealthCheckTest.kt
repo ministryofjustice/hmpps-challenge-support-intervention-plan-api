8fc5d318069ba8b1fa14bf3244d7202dedf94d08
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-12-06 14:47:31 +0000

    MIC-796 (#232)
    * MIC-796
    
    * ktlintFormat

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/integration/health/HealthCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/integration/health/HealthCheckTest.kt
@@ -1,8 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.integration.health
 
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.integration.wiremock.HmppsAuthApiExtension.Companion.hmppsAuth
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.integration.wiremock.ManageUsersExtension.Companion.manageUsers
+import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.integration.wiremock.PrisonerSearchExtension.Companion.prisonerSearch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.function.Consumer
@@ -11,6 +17,7 @@ class HealthCheckTest : IntegrationTestBase() {
 
   @Test
   fun `Health page reports ok`() {
+    setUpDependencyResponses(HttpStatus.OK)
     webTestClient.get()
       .uri("/health")
       .exchange()
@@ -18,10 +25,29 @@ class HealthCheckTest : IntegrationTestBase() {
       .isOk
       .expectBody()
       .jsonPath("status").isEqualTo("UP")
+      .jsonPath("components.authApiHealth.status").isEqualTo("UP")
+      .jsonPath("components.prisonerSearchApiHealth.status").isEqualTo("UP")
+      .jsonPath("components.manageUsersApiHealth.status").isEqualTo("UP")
+  }
+
+  @Test
+  fun `Health page reports service not available`() {
+    setUpDependencyResponses(HttpStatus.SERVICE_UNAVAILABLE)
+    webTestClient.get()
+      .uri("/health")
+      .exchange()
+      .expectStatus()
+      .isEqualTo(503)
+      .expectBody()
+      .jsonPath("status").isEqualTo("DOWN")
+      .jsonPath("components.authApiHealth.status").isEqualTo("DOWN")
+      .jsonPath("components.prisonerSearchApiHealth.status").isEqualTo("DOWN")
+      .jsonPath("components.manageUsersApiHealth.status").isEqualTo("DOWN")
   }
 
   @Test
   fun `Health info reports version`() {
+    setUpDependencyResponses(HttpStatus.OK)
     webTestClient.get().uri("/health")
       .exchange()
       .expectStatus().isOk
@@ -34,6 +60,7 @@ class HealthCheckTest : IntegrationTestBase() {
 
   @Test
   fun `Health ping page is accessible`() {
+    setUpDependencyResponses(HttpStatus.OK)
     webTestClient.get()
       .uri("/health/ping")
       .exchange()
@@ -45,6 +72,7 @@ class HealthCheckTest : IntegrationTestBase() {
 
   @Test
   fun `readiness reports ok`() {
+    setUpDependencyResponses(HttpStatus.OK)
     webTestClient.get()
       .uri("/health/readiness")
       .exchange()
@@ -56,6 +84,7 @@ class HealthCheckTest : IntegrationTestBase() {
 
   @Test
   fun `liveness reports ok`() {
+    setUpDependencyResponses(HttpStatus.OK)
     webTestClient.get()
       .uri("/health/liveness")
       .exchange()
@@ -63,5 +92,20 @@ class HealthCheckTest : IntegrationTestBase() {
       .isOk
       .expectBody()
       .jsonPath("status").isEqualTo("UP")
+  }
+
+  fun setUpDependencyResponses(responseStatus: HttpStatus) {
+    val dependencies = listOf(hmppsAuth, manageUsers, prisonerSearch)
+    dependencies.forEach {
+      val url = if (it == hmppsAuth) "/auth/health/ping" else "/health/ping"
+      it.stubFor(
+        get(url).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(if (responseStatus == HttpStatus.OK) "{\"status\":\"UP\"}" else "some error")
+            .withStatus(responseStatus.value()),
+        ),
+      )
+    }
   }
 }
