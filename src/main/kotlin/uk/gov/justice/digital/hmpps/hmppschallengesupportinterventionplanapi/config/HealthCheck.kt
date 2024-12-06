6bf6e6131eb5316dad4388dc94a23a6485eb861b
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-12-06 14:31:41 +0000

    ktlintFormat

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/config/HealthCheck.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/config/HealthCheck.kt
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
-import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.client.retryIdempotentRequestOnTransientException
 
 abstract class HealthCheck(private val webClient: WebClient) : HealthIndicator {
   override fun health(): Health? =
