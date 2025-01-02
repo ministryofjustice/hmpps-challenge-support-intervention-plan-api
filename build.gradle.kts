70692aa1dadbd38b9ac1e101058bd581fa5f07e4
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-01-02 13:12:16 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
-  kotlin("plugin.spring") version "2.1.0"
-  kotlin("plugin.jpa") version "2.1.0"
+  kotlin("plugin.spring") version "2.0.21"
+  kotlin("plugin.jpa") version "2.0.21"
   id("com.google.cloud.tools.jib") version "3.4.4"
   jacoco
 }
