20551d90696870e25c91514d901a0f50e66c3912
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-05-23 09:19:33 +0100

    Upgrade spring boot version (#310)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
   kotlin("plugin.spring") version "2.1.21"
   kotlin("plugin.jpa") version "2.1.21"
   id("com.google.cloud.tools.jib") version "3.4.5"
