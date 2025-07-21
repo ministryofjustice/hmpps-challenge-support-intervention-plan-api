17233b0e6715fca2acfc87cc807e7609708f61f6
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-07-21 10:10:02 +0100

    Hide reconciliation endpoint and adjust cron job (#332)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/controller/ReconciliationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/controller/ReconciliationController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +10,7 @@ import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.ser
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 class ReconciliationController(private val reconciliationInitiator: ReconciliationInitiator) {
+  @Operation(hidden = true)
   @PostMapping("/reconciliation/person")
   fun personReconciliation() {
     reconciliationInitiator.initiatePersonReconciliation()
