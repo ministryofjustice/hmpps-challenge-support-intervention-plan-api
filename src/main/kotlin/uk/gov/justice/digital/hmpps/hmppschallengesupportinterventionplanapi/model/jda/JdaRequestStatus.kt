99f172bf3b9f9511ca25ca3830a28e61dddf4460
Adnan Haybe <adnan.haybe@justice.gov.uk>
2026-07-21 16:25:08 +0100

    JDA-400 create JDA client abstraction and supporting models (#411)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/jda/JdaRequestStatus.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.model.jda
+
+enum class JdaRequestStatus {
+  QUEUED,
+  PROCESSING,
+  SUCCEEDED,
+  FAILED,
+  REJECTED,
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/jda/JdaRequestType.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.model.jda
+
+enum class JdaRequestType {
+  SYNC,
+  ASYNC,
+}
