0a554816292e64c4a6ce96c137d0fe33c3b3ef39
Adnan Haybe <adnan.haybe@justice.gov.uk>
2026-07-27 09:54:59 +0100

    JDA-361 Trigger CSIP Assist JDA analysis for investigation outcomes (#412)
    * JDA-361 Trigger CSIP Assist case note analysis for investigations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/jda/JdaRequestStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/jda/JdaRequestStatus.kt
@@ -1,9 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.model.jda
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class JdaRequestStatus {
+  @JsonProperty("queued")
   QUEUED,
+
+  @JsonProperty("processing")
   PROCESSING,
+
+  @JsonProperty("succeeded")
   SUCCEEDED,
+
+  @JsonProperty("failed")
   FAILED,
+
+  @JsonProperty("rejected")
   REJECTED,
 }
