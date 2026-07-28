5e7661e254c6e41609255cc0f21d07a22f02f617
GCheema <gurnank.cheema@justice.gov.uk>
2026-07-28 16:11:42 +0100

    JDA-404 record id will be generated outside db. renamed column names and sizes

--- src/main/resources/migration/common/V64__add_case_note_annotations_table.sql
+++ src/main/resources/migration/common/V64__add_case_note_annotations_table.sql
@@ -1,20 +1,18 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 CREATE TABLE case_note_annotations
 (
-    id                      UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    id                      UUID PRIMARY KEY NOT NULL,
     request_id              UUID,
-    prisoner_id             VARCHAR(255),
+    prisoner_number         VARCHAR(10),
     case_note_id            UUID,
     prompt_key              VARCHAR(255),
     prompt_version          INT,
-    behaviour_type          VARCHAR(255),
+    behaviour_type          VARCHAR(30),
     confidence_level        VARCHAR(10),
     annotated_text          TEXT,
     created_date            TIMESTAMP
 );
 
 CREATE INDEX idx_case_note_annotations_request_id ON case_note_annotations(request_id);
-CREATE INDEX idx_case_note_annotations_prisoner_id ON case_note_annotations(prisoner_id);
+CREATE INDEX idx_case_note_annotations_prisoner_number ON case_note_annotations(prisoner_number);
 CREATE INDEX idx_case_note_annotations_case_note_id ON case_note_annotations(case_note_id);
 
