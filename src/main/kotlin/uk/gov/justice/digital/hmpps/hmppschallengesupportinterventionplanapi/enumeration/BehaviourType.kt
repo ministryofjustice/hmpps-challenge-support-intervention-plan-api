c96cf9943037ffdf784ac7daeffdf01ebd26873c
Adnan Haybe <adnan.haybe@justice.gov.uk>
2026-07-29 14:50:14 +0100

    update BehaviourType to pass strings instead of integer values

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/enumeration/BehaviourType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/enumeration/BehaviourType.kt
@@ -5,17 +5,17 @@ import com.fasterxml.jackson.annotation.JsonValue
 
 enum class BehaviourType(
   @JsonValue
-  val value: Int,
+  val value: String,
 ) {
-  RISKS_AND_TRIGGERS(0),
-  USUAL_BEHAVIOUR_PRESENTATION(1),
-  PROTECTIVE_FACTORS(2),
+  RISKS_AND_TRIGGERS("risks_and_triggers"),
+  USUAL_BEHAVIOUR_PRESENTATION("usual_behaviour_presentation"),
+  PROTECTIVE_FACTORS("protective_factors"),
   ;
 
   companion object {
     @JsonCreator
     @JvmStatic
-    fun from(value: Int): BehaviourType = entries.firstOrNull { it.value == value }
+    fun from(value: String): BehaviourType = entries.firstOrNull { it.value.equals(value, ignoreCase = true) }
       ?: throw IllegalArgumentException("Unknown BehaviourType value: $value")
   }
 }
