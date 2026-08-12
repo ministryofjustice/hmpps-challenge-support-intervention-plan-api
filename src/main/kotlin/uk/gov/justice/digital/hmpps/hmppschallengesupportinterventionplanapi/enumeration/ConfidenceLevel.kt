7309f583e58382471a5d765c3bfa8f7392086d7e
Adnan Haybe <adnan.haybe@justice.gov.uk>
2026-08-12 11:06:24 +0100

    match ConfidenceLevel implementation to match BehaviourType enum class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/enumeration/ConfidenceLevel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/enumeration/ConfidenceLevel.kt
@@ -1,7 +1,21 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration
 
-enum class ConfidenceLevel {
-  LOW,
-  MEDIUM,
-  HIGH,
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class ConfidenceLevel(
+  @JsonValue
+  val value: String,
+) {
+  LOW("low"),
+  MEDIUM("medium"),
+  HIGH("high"),
+  ;
+
+  companion object {
+    @JsonCreator
+    @JvmStatic
+    fun from(value: String): ConfidenceLevel = entries.firstOrNull { it.value.equals(value, ignoreCase = true) }
+      ?: throw IllegalArgumentException("Unknown ConfidenceLevel value: $value")
+  }
 }
