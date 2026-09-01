0e15b82c594e1c216db729cb6b32aceb104aa196
GCheema <gurnank.cheema@justice.gov.uk>
2026-09-01 12:57:57 +0100

    JDA-567 linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/service/CaseNoteAnnotationsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/service/CaseNoteAnnotationsService.kt
@@ -27,7 +27,7 @@ class CaseNoteAnnotationsService(
 
   fun processQueuedCaseNoteAnnotations() {
     val response = jdaClient.getCaseNoteAnnotationsFromQueue()
-    log.info("Dequeued case note annotations for request ${response?.requestId} is ${response}")
+    log.info("Dequeued case note annotations for request ${response?.requestId} is $response")
     persistAnnotationsFromDequeue(response)
   }
 
