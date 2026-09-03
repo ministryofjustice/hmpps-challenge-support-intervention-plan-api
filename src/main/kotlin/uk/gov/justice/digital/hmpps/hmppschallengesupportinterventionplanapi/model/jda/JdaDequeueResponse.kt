92316af739567029cd2e34e4ca7cdafd5ecca59a
GCheema <gurnank.cheema@justice.gov.uk>
2026-09-03 12:43:49 +0100

    JDA-567 linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/jda/JdaDequeueResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/jda/JdaDequeueResponse.kt
@@ -6,7 +6,6 @@ import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enu
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration.ConfidenceLevel
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration.JdaDequeueResponseStatus
 import java.time.LocalDateTime
-import java.time.OffsetDateTime
 import java.util.UUID
 
 data class JdaDequeueResponse(
