6d3865f2fc126bf298f8bf3416520c16a967de32
Adnan Haybe <adnan.haybe@justice.gov.uk>
2026-09-07 10:15:46 +0100

    remove snake_case

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/SuggestedCaseNotes.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/SuggestedCaseNotes.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration.BehaviourType
 import java.time.LocalDateTime
@@ -18,10 +17,7 @@ data class SuggestedCaseNotesResponse(
 @Schema(description = "A single suggested case note")
 data class SuggestedCaseNote(
   val relevance: String,
-  @JsonProperty("case_note_id")
   val caseNoteId: UUID,
-  @JsonProperty("created_at")
   val createdAt: LocalDateTime,
-  @JsonProperty("annotated_case_note")
   val annotatedCaseNote: String,
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/controller/SuggestedCaseNotesControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/controller/SuggestedCaseNotesControllerIntTest.kt
@@ -213,10 +213,10 @@ class SuggestedCaseNotesControllerIntTest : IntegrationTestBase() {
       .expectBody()
       .jsonPath("$.prisonerNumber").isEqualTo(prisonerNumber)
       .jsonPath("$.behaviourType").isEqualTo("risks_and_triggers")
-      .jsonPath("$.suggestedCaseNotes[0].case_note_id").isEqualTo(caseNoteId.toString())
-      .jsonPath("$.suggestedCaseNotes[0].created_at").isEqualTo("2026-07-09T15:30:00")
+      .jsonPath("$.suggestedCaseNotes[0].caseNoteId").isEqualTo(caseNoteId.toString())
+      .jsonPath("$.suggestedCaseNotes[0].createdAt").isEqualTo("2026-07-09T15:30:00")
       .jsonPath("$.suggestedCaseNotes[0].relevance").isEqualTo("high")
-      .jsonPath("$.suggestedCaseNotes[0].annotated_case_note").value<String> {
+      .jsonPath("$.suggestedCaseNotes[0].annotatedCaseNote").value<String> {
         assertThat(it).contains("<span class=\"annotation-type\">became agitated</span>")
       }
   }
