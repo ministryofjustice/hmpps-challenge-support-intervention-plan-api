ab40099f69f6a281d91a8b57c4de7d3a2b85e5bd
Adnan Haybe <adnan.haybe@justice.gov.uk>
2026-09-07 10:33:12 +0100

    JDA-580 Add Date to SuggestedCaseNote (#464)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/SuggestedCaseNotes.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/model/SuggestedCaseNotes.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.hmppschallengesupportinterventionplanapi.enumeration.BehaviourType
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Schema(description = "Suggested case notes response")
@@ -17,8 +17,7 @@ data class SuggestedCaseNotesResponse(
 @Schema(description = "A single suggested case note")
 data class SuggestedCaseNote(
   val relevance: String,
-  @JsonProperty("case_note_id")
   val caseNoteId: UUID,
-  @JsonProperty("annotated_case_note")
+  val createdAt: LocalDateTime,
   val annotatedCaseNote: String,
 )
