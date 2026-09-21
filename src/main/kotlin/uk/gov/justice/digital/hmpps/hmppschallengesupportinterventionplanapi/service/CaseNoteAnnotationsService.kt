89da7cccf82108bff286e0ad47aa28f5aec48389
Mohammed Debashi <mdebashi@gmail.com>
2026-09-21 15:28:07 +0100

    JDA-653 - display casenote author (#474)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/service/CaseNoteAnnotationsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppschallengesupportinterventionplanapi/service/CaseNoteAnnotationsService.kt
@@ -94,6 +94,7 @@ class CaseNoteAnnotationsService(
           relevance = highestConfidence.value,
           caseNoteId = caseNoteWithAnnotations.caseNote.caseNoteId,
           createdAt = caseNoteWithAnnotations.caseNote.creationDateTime,
+          createdBy = caseNoteWithAnnotations.caseNote.authorName,
           annotatedCaseNote = composeCaseNoteAnnotation(caseNoteWithAnnotations, appliedSortOrder),
           amendments = composeAmendmentAnnotations(caseNoteWithAnnotations),
         )
